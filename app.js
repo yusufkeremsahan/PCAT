66d908f4b12252c012f903bbc8a546456dfd9321
Ashforfian <yusufkerem2607@gmail.com>
2025-10-07 22:20:30 +0300

    Static files appended

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const port = 3000;
 const app = express();
+
+app.use(express.static('public'));
 
 app.listen(port, () =>{
     console.log(`Sunucu ${port} portunda çalışıyor..`);
@@ -8,5 +15,5 @@ app.listen(port, () =>{
 
 
 app.get('/', (req, res)=>{
-    res.send("<h1>Merhaba</h1>");
+    res.sendFile(path.resolve(__dirname ,"temp/index.html"));
 })

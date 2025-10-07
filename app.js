c10ff83c30d055236f218c0ff98c90c209f8541f
Ashforfian <yusufkerem2607@gmail.com>
2025-10-07 21:09:40 +0300

    Add nodemon and update package.json

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log("Merhaba yeni çalışma ortamı");
+import express from 'express';
+const port = 3000;
+const app = express();
+
+app.listen(port, () =>{
+    console.log(`Sunucu ${port} portunda çalışıyor..`);
+})
+
+
+app.get('/', (req, res)=>{
+    res.send("<h1>Merhaba</h1>");
+})

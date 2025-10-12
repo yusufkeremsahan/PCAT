ff92a724ded0f0730e2fd07e112f7d8f90c2ba1e
Ashforfian <yusufkerem2607@gmail.com>
2025-10-12 23:44:06 +0300

    EJS is used

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 import express from 'express';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import ejs from 'ejs';
+
+
+
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const port = 3000;
 const app = express();
+
+
+//Template Engine
+app.set("view engine","ejs");
 
 app.use(express.static('public'));
 
@@ -15,5 +23,13 @@ app.listen(port, () =>{
 
 
 app.get('/', (req, res)=>{
-    res.sendFile(path.resolve(__dirname ,"temp/index.html"));
+    res.render('index');
+})
+
+app.get('/about', (req, res)=>{
+    res.render('about');
+})
+app.get('/add', (req, res)=>{
+    //res.sendFile(path.resolve(__dirname ,"temp/index.html"));
+    res.render('add');
 })

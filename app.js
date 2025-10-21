98ece29192131cd75f0557ed922f1bef4f985561
Ashforfian <yusufkerem2607@gmail.com>
2025-10-21 13:42:00 +0300

    Enabled image adding functionality.

--- app.js
+++ app.js
@@ -4,6 +4,14 @@ import { fileURLToPath } from 'url';
 import ejs from 'ejs';
 import Photo from'./models/Photo.js'; 
 import mongoose from 'mongoose';
+import fileUpload from 'express-fileupload';
+import fs from 'fs';
+
+const uploadDir = 'public/uploads';
+
+if(!fs.existsSync(uploadDir)){
+    fs.mkdirSync(uploadDir);
+}
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -16,9 +24,11 @@ mongoose.connect('mongodb://localhost/pcat-test-db');
 //Template Engine
 app.set("view engine","ejs");
 
+//MIDDLEWARES
 app.use(express.static('public'));
-app.use(express.urlencoded({extended:true}))
-app.use(express.json())
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+app.use(fileUpload());
 
 app.listen(port, () =>{
     console.log(`Sunucu ${port} portunda çalışıyor..`);
@@ -26,13 +36,24 @@ app.listen(port, () =>{
 
 
 app.post('/photos', async (req,res)=>{
-    Photo.create(req.body);
-    return res.redirect('/');
+    //console.log(req.files.image);
+    //Photo.create(req.body);
+    //return res.redirect('/');
+
+    let uploadImage= req.files.image;
+    let uploadPath = __dirname + '/public/uploads/' + uploadImage.name;
+    uploadImage.mv(uploadPath,async () =>{
+        await Photo.create({
+            ...req.body,
+            image: '/uploads/' + uploadImage.name
+        })
+        res.redirect('/');
+    }
+    )
 })
 
-
 app.get('/', async (req, res)=>{
-    const photos = await Photo.find({})
+    const photos = await Photo.find({}).sort('-dateCreated');
     res.render('index', {photos});
 })
 
